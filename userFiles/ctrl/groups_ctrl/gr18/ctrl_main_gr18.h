38673fad5f76860722c6c3e01df2b9ae12c0e534
Bananouille <quentinfrancois0@gmail.com>
2016-10-17 13:21:17 +0200

    triangulation 2/2

---
+++ userFiles/ctrl/groups_ctrl/gr18/ctrl_main_gr18.h
@@ -0,0 +1,21 @@
+/*! 
+ * \file ctrl_main_gr18.h
+ * \brief main header of the controller
+ */
+
+#ifndef _CTRL_MAIN_GR18_H_
+#define _CTRL_MAIN_GR18_H_
+
+#include "CtrlStruct_gr18.h"
+#include "namespace_ctrl.h"
+#include <stdlib.h>
+
+NAMESPACE_INIT(ctrlGr18);
+
+void controller_init(CtrlStruct *cvs);
+void controller_loop(CtrlStruct *cvs);
+void controller_finish(CtrlStruct *cvs);
+
+NAMESPACE_CLOSE();
+
+#endif
